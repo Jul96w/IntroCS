329ee29a339b3f44551473fdd5cb4ef13c6cdc09
Julian Weishaupt <julian.weishaupt@gmail.com>
2020-04-27 21:57:45 +0200

    Updated loop solution

--- Module 1/loops.c
+++ Module 1/loops.c
@@ -3,16 +3,35 @@
 
 int main(void)
 {
-    for (int i = 1, j = 0; i < 6; i++)
+    for (int i = 0, j = 0; j < 5; j++)
     {
-        if (j < 5)
-        {i = i - 1;}
-        if (j == 5)
-        {j = 0;}
         if (i == 5)
         {return 0;}
-        printf("i = %d, j = %d\n", i, j);
-        j = (j + 1) % 6;
 
+        printf("i = %d, j = %d\n", i, j);
+        if (j == 4)
+        {
+            i++;
+            j = -1;
+        }
     }   
 }
+
+
+// -------------------------- OLD SOLUTION ----------------------------------- 
+// 
+// int main(void)
+// {
+//     for (int i = 1, j = 0; i < 6; i++)
+//     {
+//         if (j < 5)
+//         {i = i - 1;}
+//         if (j == 5)
+//         {j = 0;}
+//         if (i == 5)
+//         {return 0;}
+//         printf("i = %d, j = %d\n", i, j);
+//         j = (j + 1) % 6;
+
+//     }   
+// }
